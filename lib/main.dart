a062ff3643a6168cac13dbe1adbf25612a551323
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-08-17 13:54:34 +0300

    FirstCommit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DecorizeApp());
+}
+
+class DecorizeApp extends StatelessWidget {
+  const DecorizeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
