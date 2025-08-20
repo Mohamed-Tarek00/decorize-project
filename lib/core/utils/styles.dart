878cfb341fbc4c93c3237fb6b7bd2468f4bb06fa
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-08-20 20:56:04 +0300

    making_font_responsive

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,33 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class Styles {
   static final TextStyle textStyle28 = TextStyle(
     fontFamily: 'IBMPlexSansArabic',
-    fontSize: 28,
+    fontSize: 28.sp,
     fontWeight: FontWeight.w600,
   );
 
   static final TextStyle textStyle20 = TextStyle(
     fontFamily: 'IBMPlexSansArabic',
-    fontSize: 20,
+    fontSize: 20.sp,
     fontWeight: FontWeight.w400,
   );
 
   static final TextStyle textStyle32 = TextStyle(
     fontFamily: 'IBMPlexSansArabic',
-    fontSize: 32,
+    fontSize: 32.sp,
     fontWeight: FontWeight.w600,
   );
 
   static final TextStyle textStyle14 = TextStyle(
     fontFamily: 'IBMPlexSansArabic',
-    fontSize: 14,
+    fontSize: 14.sp,
     fontWeight: FontWeight.normal,
   );
 
   static final TextStyle textStyle16 = TextStyle(
     fontFamily: 'IBMPlexSansArabic',
-    fontSize: 16,
+    fontSize: 16.sp,
     fontWeight: FontWeight.w500,
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:decorize_project/core/utils/app_router.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+
   runApp(
     EasyLocalization(
       supportedLocales: [Locale('en'), Locale('ar')],
       path: 'assets/translations',
       fallbackLocale: Locale('ar'),
       startLocale: Locale('ar'),
-
-      child: const DecorizeApp(),
+      child: ScreenUtilInit(
+        designSize: Size(375, 812),
+        builder: (context, child) => const DecorizeApp(),
+      ),
     ),
   );
 }
@@ -28,6 +32,11 @@ class DecorizeApp extends StatelessWidget {
       locale: context.locale,
       supportedLocales: context.supportedLocales,
       localizationsDelegates: context.localizationDelegates,
+
+      builder: (context, child) {
+        ScreenUtil.init(context);
+        return child!;
+      },
     );
   }
 }
