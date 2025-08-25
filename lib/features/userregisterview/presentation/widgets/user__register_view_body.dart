d41cfddba68994032fa155741d1d83b6390a568d
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-08-26 02:45:39 +0300

    ui_register_Screen_part_3_done

--- lib/features/userregisterview/presentation/widgets/user__register_view_body.dart
+++ lib/features/userregisterview/presentation/widgets/user__register_view_body.dart
@@ -3,10 +3,17 @@ import 'package:decorize_project/core/utils/styles.dart';
 import 'package:decorize_project/features/userregisterview/presentation/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class UserRegisterViewBody extends StatelessWidget {
+class UserRegisterViewBody extends StatefulWidget {
   const UserRegisterViewBody({super.key});
 
+  @override
+  State<UserRegisterViewBody> createState() => _UserRegisterViewBodyState();
+}
+
+class _UserRegisterViewBodyState extends State<UserRegisterViewBody> {
+  bool isAgreed = false;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -35,6 +42,28 @@ class UserRegisterViewBody extends StatelessWidget {
             name: 'تأكيد كلمة المرور',
             hintText: 'ادخل كلمة المرور',
             iconPath: 'assets/icons/lock.svg',
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.h),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isAgreed = !isAgreed;
+                    });
+                  },
+                  child: SvgPicture.asset(
+                    'assets/icons/tick-square.svg',
+                    width: 20.w,
+                    height: 20.h,
+                    color: isAgreed ? Colors.blue : Colors.grey,
+                  ),
+                ),
+                SizedBox(width: 8.w),
+                Text('الموافقة على الشروط والأحكام', style: Styles.textStyle14),
+              ],
+            ),
           ),
         ],
       ),
