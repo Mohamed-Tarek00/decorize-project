13bf07d656c29851e2a961c137c6d875ff6ce45c
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-08-31 06:12:26 +0300

    city_cubit_added

---
+++ lib/features/authantication/presentation/cubits/city_cubit/cubit/city_cubit.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+import 'package:decorize_project/features/authantication/domain/usecases/get_cities_use_case.dart';
+import 'package:decorize_project/features/authantication/presentation/cubits/city_cubit/cubit/city_state.dart';
+import 'package:meta/meta.dart';
+
+class CityCubit extends Cubit<CityState> {
+  CityCubit(this.getCitiesUseCase) : super(CityInitial());
+  final GetCitiesUseCase getCitiesUseCase;
+  Future<void> fetchCities(int governorateId) async {
+    emit(CityLoading());
+    final results = await getCitiesUseCase(governorateId);
+    results.fold(
+      (failure) => (emit(CityError(failure.toString()))),
+      (cities) => (emit(CityLoaded(cities))),
+    );
+  }
+}
---
+++ lib/features/authantication/presentation/cubits/city_cubit/cubit/city_state.dart
@@ -0,0 +1,20 @@
+import 'package:decorize_project/features/authantication/domain/entities/city.dart';
+import 'package:decorize_project/features/authantication/domain/entities/governorate.dart';
+
+abstract class CityState {}
+
+class CityInitial extends CityState {}
+
+class CityLoading extends CityState {}
+
+class CityLoaded extends CityState {
+  final List<City> cities;
+
+  CityLoaded(this.cities);
+}
+
+class CityError extends CityInitial {
+  final String message;
+
+  CityError(this.message);
+}
