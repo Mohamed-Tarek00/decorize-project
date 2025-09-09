be8f2ed338c52f37baec5503cf867e770cabb132
Mohamed Tarek <mohamedtarek@gmail.com>
2025-09-09 16:54:43 +0300

    update route names

--- lib/core/routes/app_routes_name.dart
+++ lib/core/routes/app_routes_name.dart
@@ -1,5 +1,5 @@
 class AppRoutesName {
-  static const String initialRouteName = '/1';
+  static const String initialRouteName = '/';
   static const String onBoardingView = '/onBoardingView';
   static const String otpView = '/otpView';
   static const String registerRouteName = '/registerRouteName';
