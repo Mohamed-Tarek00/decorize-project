f675369251ff2de0c238d46cc22eb9891732d50a
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-09-12 23:21:07 +0300

    choose_place_logic_and_ui_done

--- lib/features/user/home/presentation/views/widgets/choose_place.dart
+++ lib/features/user/home/presentation/views/widgets/choose_place.dart
@@ -1,12 +1,16 @@
 import 'package:decorize_project/core/utils/styles.dart';
-import 'package:decorize_project/features/user/home/presentation/views/widgets/place_model.dart';
+import 'package:decorize_project/features/user/home/presentation/views/widgets/choose_place_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/svg.dart';
 
-class ChoosePlace extends StatelessWidget {
+class ChoosePlace extends StatefulWidget {
   const ChoosePlace({super.key});
 
+  @override
+  State<ChoosePlace> createState() => _ChoosePlaceState();
+}
+
+class _ChoosePlaceState extends State<ChoosePlace> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -20,44 +24,7 @@ class ChoosePlace extends StatelessWidget {
             style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w700),
           ),
           SizedBox(height: 15.h),
-          SizedBox(
-            height: 75.h,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: PlaceModel.places.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: EdgeInsets.only(right: 6.h),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 4.w,
-                      vertical: 4.h,
-                    ),
-                    height: 75.h,
-                    width: 75.w,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.grey.shade300, width: 2),
-                      borderRadius: BorderRadius.all(Radius.circular(8.r)),
-                    ),
-                    child: Column(
-                      children: [
-                        SvgPicture.asset(
-                          PlaceModel.places[index].placeImage,
-                          color: Colors.black,
-                        ),
-                        SizedBox(height: 4.h),
-                        Text(
-                          PlaceModel.places[index].placeName,
-                          style: Styles.textStyle12,
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
+          SizedBox(height: 75.h, child: ChoosePlaceListView()),
         ],
       ),
     );
---
+++ lib/features/user/home/presentation/views/widgets/place_item.dart
@@ -0,0 +1,52 @@
+import 'package:decorize_project/core/constants.dart';
+import 'package:decorize_project/core/utils/styles.dart';
+import 'package:decorize_project/features/user/home/presentation/views/widgets/place_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+
+class PlaceItem extends StatelessWidget {
+  const PlaceItem({
+    super.key,
+    required this.index,
+    this.onTap,
+    required this.isSelectedIndex,
+  });
+  final int index;
+  final void Function()? onTap;
+  final bool isSelectedIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: EdgeInsets.only(right: 4.h),
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 4.h),
+          height: 75.h,
+          width: 75.w,
+          decoration: BoxDecoration(
+            color: isSelectedIndex ? kPrimaryColor : Colors.white,
+            border: Border.all(color: Colors.grey.shade300, width: 2),
+            borderRadius: BorderRadius.all(Radius.circular(8.r)),
+          ),
+          child: Column(
+            children: [
+              SvgPicture.asset(
+                PlaceModel.places[index].placeImage,
+                color: isSelectedIndex ? Colors.white : Colors.black,
+              ),
+              Text(
+                PlaceModel.places[index].placeName,
+                style: Styles.textStyle12.copyWith(
+                  color: isSelectedIndex ? Colors.white : Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
