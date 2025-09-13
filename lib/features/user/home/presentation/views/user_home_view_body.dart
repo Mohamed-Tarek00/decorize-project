c1b66008c91896562d153575051dfd8de280ff53
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-09-13 03:53:56 +0300

    design_photos_added

--- lib/features/user/home/presentation/views/user_home_view_body.dart
+++ lib/features/user/home/presentation/views/user_home_view_body.dart
@@ -1,6 +1,6 @@
-import 'dart:ui' as BorderType;
-
+import 'package:decorize_project/core/utils/styles.dart';
 import 'package:decorize_project/features/user/home/presentation/views/widgets/choose_place.dart';
+import 'package:decorize_project/features/user/home/presentation/views/widgets/choose_place_list_view.dart';
 import 'package:decorize_project/features/user/home/presentation/views/widgets/custom_user_app_bar.dart';
 import 'package:decorize_project/features/user/home/presentation/views/widgets/pick_multiple_images.dart';
 import 'package:flutter/material.dart';
@@ -15,10 +15,26 @@ class UserHomeViewBody extends StatelessWidget {
     return Column(
       children: [
         CustomUserAppBar(),
-        SizedBox(height: 20.h),
+        SizedBox(height: 15.h),
         PickMultipleImages(),
-        SizedBox(height: 20.h),
+        SizedBox(height: 15.h),
         ChoosePlace(),
+        SizedBox(height: 15.h),
+        Padding(
+          padding: EdgeInsets.only(right: 12.w, bottom: 12.h),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+
+            children: [
+              Text(
+                'اختر التصميم المناسب',
+                style: Styles.textStyle16.copyWith(fontWeight: FontWeight.w700),
+              ),
+              SizedBox(height: 15.h),
+              SizedBox(height: 75.h, child: Container()),
+            ],
+          ),
+        ),
       ],
     );
   }
