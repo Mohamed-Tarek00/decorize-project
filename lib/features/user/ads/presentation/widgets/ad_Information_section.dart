6a3afc9728ff4202f894536332be88ef65a2f45c
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-09-20 18:43:24 +0300

    add_informaiton_section_done

---
+++ lib/features/user/ads/presentation/widgets/ad_Information_section.dart
@@ -0,0 +1,45 @@
+import 'package:decorize_project/core/utils/styles.dart';
+import 'package:decorize_project/features/user/ads/presentation/widgets/ad_model.dart';
+import 'package:decorize_project/features/user/ads/presentation/widgets/applied_workers_section.dart';
+import 'package:decorize_project/features/user/ads/presentation/widgets/information_order_Section.dart';
+import 'package:decorize_project/features/user/ads/presentation/widgets/location_section.dart';
+import 'package:decorize_project/features/user/ads/presentation/widgets/price_Section.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AdInformationSection extends StatelessWidget {
+  const AdInformationSection({super.key, required this.ad});
+
+  final AdModel ad;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 22.w, vertical: 8.h),
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(24.r),
+            child: Image.asset(
+              ad.imagePath,
+              width: double.infinity,
+              height: 130.h,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 15.h),
+          Text('${ad.subTitle}', style: Styles.textStyle14),
+          SizedBox(height: 15.h),
+          InformationOrderSection(),
+          SizedBox(height: 15.h),
+          PriceSection(),
+          SizedBox(height: 15.h),
+          AppliedWorkersSection(),
+          SizedBox(height: 15.h),
+          LocationSection(),
+          SizedBox(height: 15.h),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/user/ads/presentation/widgets/ad_details_view_body.dart
+++ lib/features/user/ads/presentation/widgets/ad_details_view_body.dart
@@ -1,15 +1,9 @@
-import 'package:decorize_project/core/utils/styles.dart';
+import 'package:decorize_project/features/user/ads/presentation/widgets/ad_Information_section.dart';
 import 'package:decorize_project/features/user/ads/presentation/widgets/ad_model.dart';
-import 'package:decorize_project/features/user/ads/presentation/widgets/applied_workers_section.dart';
 import 'package:decorize_project/features/user/ads/presentation/widgets/custom_app_bar.dart';
-import 'package:decorize_project/features/user/ads/presentation/widgets/custom_information_widget.dart';
-import 'package:decorize_project/features/user/ads/presentation/widgets/information_order_Section.dart';
-import 'package:decorize_project/features/user/ads/presentation/widgets/location_section.dart';
 import 'package:decorize_project/features/user/ads/presentation/widgets/navigation_ad_details_buttons.dart';
-import 'package:decorize_project/features/user/ads/presentation/widgets/price_Section.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 
 class AdDetailsViewBody extends StatelessWidget {
@@ -31,33 +25,7 @@ class AdDetailsViewBody extends StatelessWidget {
           Container(width: double.infinity, height: 20.h, color: Colors.white),
           SizedBox(height: 20.h),
           NavigationAdDetailsButtons(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 22.w, vertical: 8.h),
-            child: Column(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(24.r),
-                  child: Image.asset(
-                    ad.imagePath,
-                    width: double.infinity,
-                    height: 130.h,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                SizedBox(height: 15.h),
-                Text('${ad.subTitle}', style: Styles.textStyle14),
-                SizedBox(height: 15.h),
-                InformationOrderSection(),
-                SizedBox(height: 15.h),
-                PriceSection(),
-                SizedBox(height: 15.h),
-                AppliedWorkersSection(),
-                SizedBox(height: 15.h),
-                LocationSection(),
-                SizedBox(height: 15.h),
-              ],
-            ),
-          ),
+          AdInformationSection(ad: ad),
         ],
       ),
     );
