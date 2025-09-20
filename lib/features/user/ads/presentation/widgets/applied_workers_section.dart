6849045716995db636db49bf1a9ce89799d23c80
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-09-20 18:21:07 +0300

    applied_workers_section_done.dart

--- lib/features/user/ads/presentation/widgets/applied_workers_section.dart
+++ lib/features/user/ads/presentation/widgets/applied_workers_section.dart
@@ -1,6 +1,7 @@
 import 'package:decorize_project/features/user/ads/presentation/widgets/custom_information_widget.dart';
 import 'package:decorize_project/features/user/ads/presentation/widgets/worker_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppliedWorkersSection extends StatelessWidget {
   const AppliedWorkersSection({super.key});
@@ -8,7 +9,11 @@ class AppliedWorkersSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CustomInformationWidget(
-      content: [WorkerCard()],
+      content: [
+        WorkerCard(),
+        Divider(color: Colors.grey.shade200, thickness: 1.25.h),
+        WorkerCard(),
+      ],
       iconPath: 'assets/icons/profile.svg',
       title: 'العامل',
     );
