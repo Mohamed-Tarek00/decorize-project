35b409c4c74e618253b2aad9fdda24c6109b60de
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-09-24 23:54:52 +0300

    add_ad_appbar_done

--- lib/features/user/add_ad/presentation/widgets/add_ad_view_body.dart
+++ lib/features/user/add_ad/presentation/widgets/add_ad_view_body.dart
@@ -1,10 +1,25 @@
+import 'package:decorize_project/features/user/ads/presentation/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AddAdViewBody extends StatelessWidget {
   const AddAdViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: []);
+    return Column(
+      children: [
+        SizedBox(height: MediaQuery.of(context).padding.top),
+
+        CustomAppBar(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          headingText: 'إنشاء اعلان',
+        ),
+        Container(width: double.infinity, height: 20.h, color: Colors.white),
+      ],
+    );
   }
 }
