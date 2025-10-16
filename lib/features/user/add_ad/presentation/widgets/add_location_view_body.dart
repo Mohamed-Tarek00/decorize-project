84d11ba139fa587492e22b2ac340a3e3ffbae6e9
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-10-16 20:52:08 +0300

    add_google_map_done

--- lib/features/user/add_ad/presentation/widgets/add_location_view_body.dart
+++ lib/features/user/add_ad/presentation/widgets/add_location_view_body.dart
@@ -1,10 +1,62 @@
+import 'package:decorize_project/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class AddLocationViewBody extends StatelessWidget {
+class AddLocationViewBody extends StatefulWidget {
   const AddLocationViewBody({super.key});
 
   @override
+  State<AddLocationViewBody> createState() => _AddLocationViewBodyState();
+}
+
+class _AddLocationViewBodyState extends State<AddLocationViewBody> {
+  GoogleMapController? _mapController;
+  LatLng? _selectedLocation;
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox.expand(
+      child: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: CameraPosition(
+              target: LatLng(30.033333, 31.233334),
+              zoom: 15,
+            ),
+            onMapCreated: (controller) {
+              _mapController = controller;
+            },
+            onTap: (position) {
+              setState(() {
+                _selectedLocation = position;
+              });
+            },
+            markers: _selectedLocation != null
+                ? {
+                    Marker(
+                      markerId: const MarkerId('Selectedposition'),
+                      position: _selectedLocation!,
+                    ),
+                  }
+                : {},
+          ),
+          _selectedLocation != null
+              ? Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    width: double.infinity,
+                    color: Colors.white,
+                    child: CustomButton(
+                      onPressed: () {
+                        print(_selectedLocation);
+                      },
+                      text: 'اضف الموقع',
+                    ),
+                  ),
+                )
+              : SizedBox.shrink(),
+        ],
+      ),
+    );
   }
 }
