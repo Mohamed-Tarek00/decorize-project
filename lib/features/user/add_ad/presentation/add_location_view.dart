b292ac97e214427b50266e7a4f26c4b03b446cd1
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-10-21 18:23:54 +0300

    refactor_add_location_view_done

--- lib/features/user/add_ad/presentation/add_location_view.dart
+++ lib/features/user/add_ad/presentation/add_location_view.dart
@@ -1,7 +1,7 @@
 import 'package:decorize_project/core/constants.dart';
 import 'package:decorize_project/core/location/cubit/location_cubit_cubit.dart';
 import 'package:decorize_project/core/utils/service_locator.dart';
-import 'package:decorize_project/features/user/add_ad/presentation/widgets/add_location_view_body.dart';
+import 'package:decorize_project/features/user/add_ad/presentation/widgets/add_Location/add_location_view_body.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/features/user/add_ad/presentation/widgets/add_Location/enter_location_section.dart
+++ lib/features/user/add_ad/presentation/widgets/add_Location/enter_location_section.dart
@@ -1,96 +1,23 @@
+
+
 import 'package:decorize_project/core/constants.dart';
 import 'package:decorize_project/core/location/cubit/location_cubit_cubit.dart';
 import 'package:decorize_project/core/location/cubit/location_cubit_state.dart';
-import 'package:decorize_project/core/utils/geo_locator.dart';
 import 'package:decorize_project/core/utils/styles.dart';
 import 'package:decorize_project/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class AddLocationViewBody extends StatefulWidget {
-  const AddLocationViewBody({super.key});
-
-  @override
-  State<AddLocationViewBody> createState() => _AddLocationViewBodyState();
-}
-
-class _AddLocationViewBodyState extends State<AddLocationViewBody> {
-  GoogleMapController? _mapController;
-  LatLng? _selectedLocation;
+class EnterLocationSection extends StatelessWidget {
+  const EnterLocationSection({super.key, this.selectedLocation});
+  final LatLng? selectedLocation;
 
   @override
   Widget build(BuildContext context) {
-    final locationCubit = context.read<LocationCubit>();
-
-    return SizedBox.expand(
-      child: Stack(
-        children: [
-          GoogleMap(
-            initialCameraPosition: CameraPosition(
-              target: LatLng(30.033333, 31.233334),
-              zoom: 15,
-            ),
-            onMapCreated: (controller) {
-              _mapController = controller;
-            },
-            onTap: (position) async {
-              setState(() {
-                _selectedLocation = position;
-              });
-              locationCubit.getLocation(
-                latitude: position.latitude,
-                longitude: position.longitude,
-              );
-            },
-            markers: _selectedLocation != null
-                ? {
-                    Marker(
-                      markerId: const MarkerId('Selectedposition'),
-                      position: _selectedLocation!,
-                    ),
-                  }
-                : {},
-          ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              width: double.infinity,
-              color: Colors.white,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-
-                children: [
-                  SizedBox(height: MediaQuery.of(context).padding.top),
-                  CustomButton(
-                    onPressed: () async {
-                      final position = await SetLocation.getLocation();
-                      setState(() {
-                        if (position != null) {
-                          _selectedLocation = LatLng(
-                            position.latitude,
-                            position.longitude,
-                          );
-                          _mapController?.animateCamera(
-                            CameraUpdate.newLatLngZoom(_selectedLocation!, 15),
-                          );
-                          locationCubit.getLocation(
-                            latitude: position.latitude,
-                            longitude: position.longitude,
-                          );
-                        }
-                      });
-                    },
-                    text: 'تفقد موقعي',
-                  ),
-                ],
-              ),
-            ),
-          ),
-          _selectedLocation != null
-              ? Align(
+    return Align(
                   alignment: Alignment.bottomCenter,
                   child: Container(
                     width: double.infinity,
@@ -114,7 +41,7 @@ class _AddLocationViewBodyState extends State<AddLocationViewBody> {
                                   vertical: 8.h,
                                 ),
                                 child: Row(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
                                     SvgPicture.asset(
                                       'assets/icons/location.svg',
@@ -122,14 +49,14 @@ class _AddLocationViewBodyState extends State<AddLocationViewBody> {
                                       height: 24.h,
                                       color: kPrimaryColor,
                                     ),
-                                    SizedBox(width: 6.w), 
+                                    SizedBox(width: 6.w),
                                     Expanded(
                                       child: Text(
                                         state.location.displayName,
                                         maxLines: 2,
                                         overflow: TextOverflow.ellipsis,
                                         style: Styles.textStyle12.copyWith(
-                                          fontSize: 12.sp, 
+                                          fontSize: 12.sp,
                                           fontWeight: FontWeight.w600,
                                           height: 1.4,
                                           color: const Color(0xFF1D1B20),
@@ -154,17 +81,13 @@ class _AddLocationViewBodyState extends State<AddLocationViewBody> {
                         ),
                         CustomButton(
                           onPressed: () {
-                            print('Selected Location: $_selectedLocation');
+                            print('Selected Location: $selectedLocation');
                           },
                           text: 'أضف الموقع',
                         ),
                       ],
                     ),
                   ),
-                )
-              : SizedBox.shrink(),
-        ],
-      ),
-    );
+                );
   }
 }
