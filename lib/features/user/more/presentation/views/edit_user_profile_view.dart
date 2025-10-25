88be5eb3dcd470903678518dad148905c5c2f31a
bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-10-26 01:14:46 +0300

    validaton_added

--- lib/features/user/more/presentation/views/edit_user_profile_view.dart
+++ lib/features/user/more/presentation/views/edit_user_profile_view.dart
@@ -1,17 +1,61 @@
 import 'package:decorize_project/core/constants.dart';
+import 'package:decorize_project/core/utils/cache_helper.dart';
+import 'package:decorize_project/core/utils/service_locator.dart';
 import 'package:decorize_project/core/widgets/custom_button.dart';
 import 'package:decorize_project/features/user/more/presentation/views/widgets/edit_profile/edit_user_profile_view_body.dart';
 import 'package:flutter/material.dart';
 
-class EditUserProfileView extends StatelessWidget {
+class EditUserProfileView extends StatefulWidget {
   const EditUserProfileView({super.key});
+
+  @override
+  State<EditUserProfileView> createState() => _EditUserProfileViewState();
+}
+
+class _EditUserProfileViewState extends State<EditUserProfileView> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> _loadUserData() async {
+    final cacheHelper = getIt<CacheHelper>();
+    final cacheUser = await cacheHelper.getUserProfile();
+    if (cacheUser != null) {
+      setState(() {
+        nameController.text = cacheUser.name;
+        phoneController.text = cacheUser.phone;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    _loadUserData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kScaffoldColor,
-      body: EditUserProfileViewBody(),
-      bottomNavigationBar: CustomButton(onPressed: () {}, text: 'التعديل'),
+      body: EditUserProfileViewBody(
+        nameController: nameController,
+        phoneController: phoneController,
+        formKey: _formKey,
+      ),
+      bottomNavigationBar: CustomButton(
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(
+                content: Text('تم حفظ البيانات مؤقتًا ✅'),
+                backgroundColor: Colors.green,
+              ),
+            );
+          }
+        },
+        text: 'التعديل',
+      ),
     );
   }
 }
--- lib/features/user/more/presentation/views/widgets/edit_profile/edit_user_profile_view_body.dart
+++ lib/features/user/more/presentation/views/widgets/edit_profile/edit_user_profile_view_body.dart
@@ -1,6 +1,5 @@
-import 'package:decorize_project/core/utils/cache_helper.dart';
-import 'package:decorize_project/core/utils/service_locator.dart';
 import 'package:decorize_project/core/utils/styles.dart';
+import 'package:decorize_project/core/utils/validator.dart';
 import 'package:decorize_project/core/widgets/custom_app_bar.dart';
 import 'package:decorize_project/features/shared/auth/presentation/widgets/register_widgets/custom_text_form_field.dart';
 import 'package:decorize_project/features/user/more/presentation/views/widgets/show_profile/change_picture_section.dart';
@@ -8,7 +7,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EditUserProfileViewBody extends StatefulWidget {
-  const EditUserProfileViewBody({super.key});
+  const EditUserProfileViewBody({
+    super.key,
+    required this.nameController,
+    required this.phoneController,
+    required this.formKey,
+  });
+  final TextEditingController nameController;
+  final TextEditingController phoneController;
+  final GlobalKey<FormState> formKey;
 
   @override
   State<EditUserProfileViewBody> createState() =>
@@ -16,70 +23,57 @@ class EditUserProfileViewBody extends StatefulWidget {
 }
 
 class _EditUserProfileViewBodyState extends State<EditUserProfileViewBody> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController phoneController = TextEditingController();
-
-  Future<void> _loadUserData() async {
-    final cacheHelper = await getIt<CacheHelper>();
-    final cacheUser = await cacheHelper.getUserProfile();
-    if (cacheUser != null) {
-      setState(() {
-        nameController.text = cacheUser.name;
-        phoneController.text = cacheUser.phone;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _loadUserData();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: MediaQuery.of(context).padding.top),
-        CustomAppBar(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          headingText: 'تعديل الملف الشخصي',
-        ),
-        SizedBox(height: 10.h),
-        ChangePictureSection(),
-        Text(nameController.text, style: Styles.textStyle18),
+    return Form(
+      key: widget.formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).padding.top),
+            CustomAppBar(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              headingText: 'تعديل الملف الشخصي',
+            ),
+            SizedBox(height: 10.h),
+            ChangePictureSection(),
+            Text(widget.nameController.text, style: Styles.textStyle18),
 
-        SizedBox(height: 10.h),
-        Padding(
-          padding: EdgeInsets.all(12.r),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(12.r),
-            ),
-            child: Column(
-              children: [
-                CustomTextFormField(
-                  controller: nameController,
-                  name: 'الاسم',
-                  hintText: 'الاسم',
-                  iconPath: 'assets/icons/profile.svg',
-                  height: 50.h,
+            SizedBox(height: 10.h),
+            Padding(
+              padding: EdgeInsets.all(12.r),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12.r),
                 ),
-                CustomTextFormField(
-                  controller: phoneController,
-                  name: 'رقم الجوال',
-                  hintText: 'رقم الجوال',
-                  iconPath: 'assets/icons/phone.svg',
-                  height: 50.h,
+                child: Column(
+                  children: [
+                    CustomTextFormField(
+                      controller: widget.nameController,
+                      name: 'الاسم',
+                      hintText: 'الاسم',
+                      iconPath: 'assets/icons/profile.svg',
+                      height: 50.h,
+                      validator: Validator.nameValidator,
+                    ),
+                    CustomTextFormField(
+                      controller: widget.phoneController,
+                      name: 'رقم الجوال',
+                      hintText: 'رقم الجوال',
+                      iconPath: 'assets/icons/phone.svg',
+                      height: 50.h,
+                      validator: Validator.phoneValidator,
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
